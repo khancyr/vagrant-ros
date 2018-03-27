0b453d6c5c0fc475ef68b017686ec55621c7e05e
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-03-27 10:32:05 +0200

    Add gazebo install

--- install-prereqs-ros.sh
+++ install-prereqs-ros.sh
@@ -13,12 +13,18 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)
 # Configure your Ubuntu repositories
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+# Configure Gazebo OSRF repositories
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 sudo apt-get update
 
 # Installation
 echo "Installing ROS."
 sudo apt-get -y install ros-kinetic-ros-base
 sudo apt-get -y install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
+echo "Installing Gazebo8 with ROS binding."
+sudo apt-get -y install gazebo8 ros-kinetic-gazebo8-ros
 
 # Initialize rosdep
 sudo rosdep init
